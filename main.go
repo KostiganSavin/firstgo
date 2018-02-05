35bf630582db8e1b7d46a88f3a7093e9927da4cb
KostiganSavin <kostigan.savin@gmail.com>
2018-02-05 23:11:51 +0300

    Golang Section3 Video 25

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 	cards := newDeck()
 
-	hand, remaningCards := deal(cards, 5)
-
-	hand.print()
-	remaningCards.print()
+	fmt.Println(cards.toString())
 }
